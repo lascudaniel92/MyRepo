d4b9f17dd245c44547448b322d1e17b0b6cff52d
lascudaniel92 <43698270+lascudaniel92@users.noreply.github.com>
2021-07-21 23:18:20 +0300

    a

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const greutate = prompt('Ce greutate ai (in kilograme)?');
+const inaltime = prompt('Ce ianltime ai (in metri)?');
+const rezultat = greutate / (inaltime ** 2);
+
+
+function myFunction() {
+    alert(rezultat.toFixed(2));
+
+}
+
+function myFunction2() {
+    alert(rezultat.toFixed(2));
+}
