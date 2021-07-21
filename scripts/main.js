7c08b0ac090a0f9c36da3bc7e5582f50e1d0f69c
lascudaniel92 <43698270+lascudaniel92@users.noreply.github.com>
2021-07-22 01:06:14 +0300

    Enter

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,28 @@
 'use strict';
 
-const greutate = prompt('Ce greutate ai (in kilograme)?');
-const inaltime = prompt('Ce ianltime ai (in metri)?');
-const rezultat = greutate / (inaltime ** 2);
+//const greutate = prompt('Ce greutate ai (in kilograme)?');
+//const inaltime = prompt('Ce inaltime ai (in centimetri)?');
 
-
-function myFunction() {
-    alert(rezultat.toFixed(2));
+function rezultat() {
+    const greutate = parseInt(document.getElementById("greutate").value);
+    if (greutate === null || isNaN(greutate)) {
+        alert("Va rugam sa introduceti greutatea");
+        return;
+    }
+    if (greutate >= 321 || greutate <= 29) {
+        alert("Va rugam sa introduceti greutatea intre 30 si 320");
+        return;
+    }
+    const inaltime = parseInt(document.getElementById("inaltime").value);
+    if (inaltime === null || isNaN(inaltime)) {
+        alert("Va rugam sa introduceti inaltimea");
+        return;
+    }
+    if (inaltime >= 221 || inaltime <= 29) {
+        alert("Va rugam sa introduceti inaltimea intre 30 si 220");
+        return;
+    }
+    const rezultat = greutate / ((inaltime / 100) ** 2);
+    document.getElementById('afisaj').innerHTML = rezultat.toFixed(2);
 
 }
