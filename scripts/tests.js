8a468dcd03fbcbe6a541eff31c250adc054c6a84
lascudaniel92 <43698270+lascudaniel92@users.noreply.github.com>
2021-07-31 22:43:57 +0300

    Enter

---
+++ scripts/tests.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function numbersGenerator(count, max) {
+  let result = [];
+  for (let i = 0; i < count; i++) {
+    result = generateUnique(result, max);
+  }
+  result.sort((a, b) => a - b);
+  dataresult.innerHTML += '<div class="theresults2">' + result + '</div>';
+}
+
+function generateUnique(array, max) {
+  let rng = Math.floor(Math.random() * max + 1);
+  while (array.includes(rng)) {
+    rng = Math.floor(Math.random() * max + 1);
+  }
+  array.push(rng);
+  return array;
+}
+
+const dataresult = document.querySelector('[data-result]');
+
+const generate = document.querySelector('[data-generated]');
+generate.addEventListener('click', () => {
+  numbersGenerator(6, 49);
+});
