20fc162253d062613b17cfcf597bf324cb3d347d
lascudaniel92 <43698270+lascudaniel92@users.noreply.github.com>
2021-09-21 03:33:53 +0300

    small changes

--- 649/649.js
+++ 649/649.js
@@ -31,7 +31,7 @@ function numbersGenerator(count, max) {
   sortedLeaderboard.sort(compare);
   dataresult.innerHTML += '<div class="theresults2">' + result.join(', ') + '</div>';
   topresults.innerHTML = '';
-  for (let i = 0; i < 49; i++) {
+  for (let i = 0; i < 48; i++) {
     let number = Object.keys(sortedLeaderboard[i])[0];
     let weight = Object.values(sortedLeaderboard[i])[0];
     if (weight !== 0) {
